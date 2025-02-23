aac878997b747dac7bb60f2a793b48eb43b41ad6
ArmanJ04 <57132784+ArmanJ04@users.noreply.github.com>
2025-02-24 00:00:33 +0500

    Delete diploma directory

--- diploma/Webapp/backend/models/Prediction.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const PredictionSchema = new mongoose.Schema({
-  email: { type: String, required: true },
-  prediction: { type: Number, required: true },
-  timestamp: { type: Date, default: Date.now }, // ✅ Ensuring it's a valid Date
-});
-
-module.exports = mongoose.model("Prediction", PredictionSchema);
